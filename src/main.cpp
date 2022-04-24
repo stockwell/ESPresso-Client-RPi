b234c942dadbaf997200926dc03eb08b28408c8c
stockwell <stockywell@gmail.com>
2022-04-24 16:32:46 +1000

    Main: Improve timer accuracy and UI responsiveness

--- src/main.cpp
+++ src/main.cpp
@@ -13,22 +13,19 @@
 
 #include <unistd.h>
 #include <pthread.h>
-#include <time.h>
-#include <sys/time.h>
+#include <signal.h>
 
 #include <netdb.h>
 
 #include "EspressoUI.hpp"
 
-#define DISP_BUF_SIZE (128 * 1024)
+#define DISP_BUF_SIZE (800 * 480)
 
-static void hal_init(void);
+static void hal_init();
+static void timer_init();
 
-int main(int argc, char** argv)
+int main(int, char**)
 {
-	(void)argc; /*Unused*/
-	(void)argv; /*Unused*/
-
 	/*Initialize LVGL*/
 	lv_init();
 
@@ -36,6 +33,7 @@ int main(int argc, char** argv)
 	fbdev_init();
 
 	hal_init();
+	timer_init();
 
 	auto& settings = SettingsManager::get();
 
@@ -58,11 +56,20 @@ int main(int argc, char** argv)
 
 	settings.save();
 
+	// Yeah, nah
+	std::thread{ [&](){
+		while(1)
+		{
+			boiler->tick();
+			usleep(20000);
+		}
+	}}.detach();
+
 	/*Handle LitlevGL tasks (tickless mode)*/
 	while (1)
 	{
 		lv_timer_handler();
-		usleep(5000);
+		usleep(500);
 	}
 
 	return 0;
@@ -76,14 +83,15 @@ int main(int argc, char** argv)
  * Initialize the Hardware Abstraction Layer (HAL) for the LVGL graphics
  * library
  */
-static void hal_init(void)
+static void hal_init()
 {
 	/*A small buffer for LittlevGL to draw the screen's content*/
 	static lv_color_t buf[DISP_BUF_SIZE];
+	static lv_color_t buf2[DISP_BUF_SIZE];
 
 	/*Initialize a descriptor for the buffer*/
 	static lv_disp_draw_buf_t disp_buf;
-	lv_disp_draw_buf_init(&disp_buf, buf, NULL, DISP_BUF_SIZE);
+	lv_disp_draw_buf_init(&disp_buf, buf, buf2, DISP_BUF_SIZE);
 
 	/*Initialize and register a display driver*/
 	static lv_disp_drv_t disp_drv;
@@ -102,4 +110,16 @@ static void hal_init(void)
 	/*This function will be called periodically (by the library) to get the mouse position and state*/
 	indev_drv_1.read_cb = evdev_read;
 	lv_indev_t* mouse_indev = lv_indev_drv_register(&indev_drv_1);
+}
+
+void alarmHandler(int sig_num)
+{
+	if (sig_num == SIGALRM)
+		lv_tick_inc(5);
+}
+
+static void timer_init()
+{
+	signal(SIGALRM, alarmHandler);
+	ualarm(5000, 5000);
 }
